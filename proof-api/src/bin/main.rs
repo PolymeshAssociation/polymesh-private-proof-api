8fdb6c697f07187f1f864ad6267dfd3153de746c
mpastecki <marcin.pastecki@inndei.com>
2024-02-23 09:16:08 +0100

    Cleaned the building process

--- proof-api/src/bin/main.rs
+++ proof-api/src/bin/main.rs
@@ -23,7 +23,7 @@ async fn start_server() -> anyhow::Result<()> {
   // building address
   let port = std::env::var("PORT").unwrap_or("8080".to_string());
   let bind_address = std::env::var("BIND_ADDRESS").unwrap_or("0.0.0.0".to_string());
-  let address = format!("{}:{}", bind_addres, port);
+  let address = format!("{}:{}", bind_address, port);
 
   // Open database.
   let pool = get_db_pool().await?;
